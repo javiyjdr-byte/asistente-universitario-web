12c6cf2b0ce68830b63adb4d30fabe0f6d1cfa48
Javier Rodriguez <javi.yjdr@gmail.com>
2026-09-21 21:52:08 -0300

    fix: abrir comentarios en Gmail web

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.SITE_CONFIG = Object.freeze({
   version: "1.0.0-piloto",
   portalUrl: "https://script.google.com/macros/s/AKfycbx5r2VXIXrk7fZ4TTGEH9XYinsBF11Y8t044XSWI9ZxNuRRvaGvqJv6O7NdSnpq0LiY0g/exec",
   privacyUrl: "./privacidad.html",
-  feedbackUrl: "mailto:eduasistenteba@gmail.com?subject=Comentarios%20sobre%20el%20piloto%20del%20Asistente%20Universitario",
+  feedbackUrl: "https://mail.google.com/mail/?view=cm&fs=1&to=eduasistenteba@gmail.com&su=Comentarios%20sobre%20el%20piloto%20del%20Asistente%20Universitario",
   contactEmail: "eduasistenteba@gmail.com",
   pilotAccess: "Acceso limitado a participantes habilitados"
 });
